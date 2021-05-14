e56fc98a975792ba6a7f9289326c6a41ea98e255
FVargas4 <fvargascode@gmail.com>
2021-05-13 19:31:08 -0500

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Fer Vargas on 13/05/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
